5b5c68645d35543cbf8398c98504d748a1a57e5b
Modraut <chadmodred@gmail.com>
2021-02-10 23:43:20 +1100

    Redesign completed

--- src/common/components/Navbar/index.js
+++ src/common/components/Navbar/index.js
@@ -9,15 +9,16 @@ import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
 import resume from "../../assets/documents/Chad.Liu_CV.pdf"
 import { Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAlignJustify, faUser, faFolderOpen, faToolbox, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faAlignJustify, faAlignRight ,faUser, faFolderOpen, faToolbox, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { debounce } from "utilities/helper";
-
 
 
 
 export const Navbar = () => {
     let preScrollPos = 0;
     const [ visibleNavbar, setVisibleNavbar ] = useState(true);
+    const [ expand, setExpand ] = useState(false);
+
     const handleScroll = () =>{
         const currentScrollPos = window.pageYOffset;
 
@@ -39,35 +40,44 @@ export const Navbar = () => {
         return() => window.removeEventListener("scroll", handleScroll)
     }, [])
 
-
+    const List = () => (
+        <ul>
+            {/* <li><img src={logo} alt="logo missing" /></li> */}
+            <li><NavLink to="/home" activeClassName="active" >
+                <FontAwesomeIcon size="0.5x" icon={faUser} /><span>Home</span>
+            </NavLink></li>
+            <li><NavLink to="/portfolio">
+                <FontAwesomeIcon size="0.5x" icon={faFolderOpen} /><span>Portfolio</span>
+            </NavLink></li>
+            <li><NavLink to={resume} target="_blank" >
+                <FontAwesomeIcon size="0.5x" icon={faToolbox} /><span>Resume</span>
+            </NavLink></li>
+            <li><NavLink to="/contact-me" >
+                <FontAwesomeIcon size="0.5x" icon={faEnvelope} /><span>Contact Me</span>
+            </NavLink></li>
+        </ul>
+    )
 
     return(
         <Fragment>
             <header className={visibleNavbar? "navbar":"navbar hide" }>
                 <nav>
-                    <ul>
-                        {/* <li><img src={logo} alt="logo missing" /></li> */}
-                        <li><NavLink to="/home" activeClassName="active" >
-                            <FontAwesomeIcon size="0.5x" icon={faUser} /><span>Home</span>
-                        </NavLink></li>
-                        <li><NavLink to="/portfolio">
-                            <FontAwesomeIcon size="0.5x" icon={faFolderOpen} /><span>Portfolio</span>
-                        </NavLink></li>
-                        <li><NavLink to={resume} target="_blank" >
-                            <FontAwesomeIcon size="0.5x" icon={faToolbox} /><span>Resume</span>
-                        </NavLink></li>
-                        <li><NavLink to="/contact-me" >
-                            <FontAwesomeIcon size="0.5x" icon={faEnvelope} /><span>Contact Me</span>
-                        </NavLink></li>
-                    </ul>
+                    <List />
                 </nav>
             </header>
-            <div className="miniNav" >
-                <FontAwesomeIcon
-                    size='sm' 
-                    icon={faAlignJustify}
-                />
-            </div>
+            <header className={expand? "miniNav expand": "miniNav" }>
+                <nav>
+                    <div className="miniNav__round__first">
+                        <FontAwesomeIcon
+                            size='sm' 
+                            icon={expand? faAlignRight: faAlignJustify}
+                            onClick={()=>setExpand(!expand)}
+                        />
+                    </div>
+                    <div className="miniNav__round__second"></div>
+                    <List />
+                </nav>
+            </header>
         </Fragment>
     )
 }
